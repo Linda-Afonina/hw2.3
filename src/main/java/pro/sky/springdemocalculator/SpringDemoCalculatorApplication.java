06ef8f0e630731652b7316a396d9de164c96c4d2
linda <charming.linda85@gmail.com>
2024-05-03 10:57:35 +0300

    Initial commit

---
+++ src/main/java/pro/sky/springdemocalculator/SpringDemoCalculatorApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.springdemocalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemoCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/springdemocalculator/SpringDemoCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.springdemocalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
